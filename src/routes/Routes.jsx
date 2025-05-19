0d0e805ac7d85c6c7141a49c96b079955689b49d
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-19 22:44:39 +0600

    project setup with tailwind,daisyUI and basic route setup with react-router

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,11 @@
+import {
+  createBrowserRouter
+} from "react-router";
+import Root from "../layouts/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>
+  },
+]);
