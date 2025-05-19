91f0b204a295258c4e1350827b060292a6b5751f
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-05-19 23:02:22 +0600

    basic navbar and footer designed

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../components/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
-           Hello World
+          <Navbar/>
+          <Outlet/>
+          <Footer/>
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,10 +2,17 @@ import {
   createBrowserRouter
 } from "react-router";
 import Root from "../layouts/Root";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>
+    element: <Root/>,
+    children: [
+        {
+            index: true,
+            Component: Home
+        }
+    ]
   },
 ]);
